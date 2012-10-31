446d7677d52332fc205750ed2c2fe9621f752222
ahmed <ahmed@ahmed-virtual-machine.(none)>
2012-10-31 05:54:18 +0300

    added the map - debug later

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 import webapp2
 from google.appengine.ext import db
-
+import urllib2
+from xml.dom import minidom
 import jinja2
 
 import os
@@ -10,11 +11,26 @@ import os
 templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
 
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(templates_dir), autoescape = True)
-
+def get_location(ip):
+    ip = '4.2.2.2'
+    url = 'http://api.hostip.info/?ip=%s'
+    if ip:
+        contnet = None
+        try:
+            content = urllib2.urlopen(url % ip).read()
+        except urllib2.URLError:
+            return        
+        if contnet:
+            d = minidom.parseStrin(content)
+            coords =d.getElementsByTageName('gml:coordinates')
+            lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+            return lat, lon
+            
 class Art(db.Model):
     title = db.StringProperty(required=True)
     art = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now=True)
+    location = db.GeoPtProperty()
 
 class BaseHandler(webapp2.RequestHandler):
     def render_str(self, template, **params):
@@ -24,11 +40,25 @@ class BaseHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.response.out.write(self.render_str(template, **kw))
    
-
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_url(points):
+    temp = GMAPS_URL
+    for p in points:
+        temp += 'markers=%s,%s&' % (p.lat, p.lon)
+    return temp[:len(temp) -1]
+    
 class AsciiChanHandler(BaseHandler):
     def get(self):
         self.arts = db.GqlQuery('select * from Art order by created desc')
-        self.render("main-page.html", arts = self.arts)
+        
+        self.arts = list(self.arts)
+        
+        points = filter(None, (a.location for a in self.arts))
+        
+        image_url = None
+        if points:
+            image_url = gmaps_url(points)
+        self.render("main-page.html", arts = self.arts, image_url = image_url)
     
     def post(self):
         self.title = self.request.get('title')
@@ -36,6 +66,11 @@ class AsciiChanHandler(BaseHandler):
         
         if self.title and self.art_text:
             art = Art(title = self.title, art = self.art_text)
+            loc = get_location(self.request.remote_addr)
+            
+            if loc:
+                art.location = db.GeoPt(loc.lat, loc.lon)
+            
             art.put()
             self.redirect('/')
         else:
